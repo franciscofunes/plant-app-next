726a571f49d2fc02fb3c9fa0b4e68765f66f4d06
Francisco Funes <ffunes@hexacta.com>
2021-11-30 20:32:48 -0300

    fix: duplicating map

--- components/plants/PlantList.js
+++ components/plants/PlantList.js
@@ -4,18 +4,15 @@ import classes from "./PlantList.module.css";
 function PlantList(props) {
   return (
     <ul className={classes.list}>
-      {props.plants.map((plant) =>
-        props.water.map((water) => (
-          <PlantItem
-            key={plant.id}
-            id={plant.id}
-            idWatering={water.id}
-            image={plant.image}
-            title={plant.title}
-            family={plant.family}
-          />
-        ))
-      )}
+      {props.plants.map((plant) => (
+        <PlantItem
+          key={plant.id}
+          id={plant.id}
+          image={plant.image}
+          title={plant.title}
+          family={plant.family}
+        />
+      ))}
     </ul>
   );
 }
